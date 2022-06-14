f42bd070949bcec2b5acb7d45d7121a4ecbfc8d4
dharmapradhan2 <dharma.pradhan@nettantra.net>
2022-06-14 18:30:11 +0530

    commit

--- redux-todo/src/Component/Header.jsx
+++ redux-todo/src/Component/Header.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 function Header() {
   return (
-    <div className="container bg-light">
-        
+    <div className="container bg-light m-2">
+        <div className="h3 text-center text-primary">Redux Todo App</div>
       <div className="form">
         <input type="text" className="form-control form-control-sm"></input>
       </div>
